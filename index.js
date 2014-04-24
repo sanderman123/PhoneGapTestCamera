dfd95bd89b29d943a58215e5842b570506d3ac97
sanderman123 <sanderman123github@hotmail.com>
2014-04-24 14:03:14 +0800

    asdf

---
+++ index.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	$("#blue").prepend('<h1 id="takePicture">Take a Picture!!</h1>');
+
+	$(document).click(function(event){
+		
+		var target = $( event.target );
+		if(target.is("div")){
+			navigator.notification.vibrate(500);
+
+			if(target.is("#blue")){				
+				navigator.camera.getPicture(onSuccess, onFail, { quality: 50, destinationType: Camera.DestinationType.FILE_URI });				
+				
+				function onSuccess(imageURI) {				   				   
+				   if("#red").is(":hidden")){
+						$("#red").fadeIn("slow");
+					}
+				   $("#red").prepend('<img id="theImg" src= ' +imageURI+' height="180" width="100">');
+				}
+
+				function onFail(message) {
+					console.log("dddd");
+					alert('Failed because: ' + message);
+				}
+
+			} else {
+				target.fadeOut("slow",function(){						
+				});				
+			}
+		} else {
+			$("#red").remove("#theImg");
+			$("div").fadeIn("slow");	
+		}
+	})			
+});
