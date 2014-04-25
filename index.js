275aa0069573211845d43fad26b03c443dfa417b
sanderman123 <sanderman123github@hotmail.com>
2014-04-25 23:48:15 +0800

    zxczxc

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ $(document).ready(function(){
 					console.log("dddd");
 					alert('Failed because: ' + message);
 				}
+			} else if(target.is("yellow")) {
+				$("#red").prepend('<img id="theImg2" src="logo.png" />');
 			} else {
 				//$("body").prepend('<img id="theImg" src="img/logo.png" />');
 				//$("body").css("background-image", url("img/logo.png"));
