974d2b47405dd1229faf6f7ba5bb1519ef72f986
sanderman123 <sanderman123github@hotmail.com>
2014-04-25 23:32:13 +0800

    Index.js copied from previous working version

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
 $(document).ready(function(){
-	$("#blue").prepend('<h1 id="takePicture">Take a Picture!!</h1>');
-
 	$(document).click(function(event){
-		
+		$("#red").css("background","img/logo.png");
+		//$("#red").prepend('<img id="theImg" src="img/logo.png" />');
+
+
 		var target = $( event.target );
 		if(target.is("div")){
-			navigator.notification.vibrate(500);
-
-			if(target.is("#blue")){			
-				//	
-				navigator.camera.getPicture(onSuccess, onFail, { quality: 50, destinationType: Camera.DestinationType.FILE_URI });				
-				
-				function onSuccess(imageURI) {				   				   
-				   if("#red").is(":hidden")){
-						$("#red").fadeIn("slow");
-					}
+			if(target.is("#red")){
+				navigator.notification.vibrate(500);
+				console.log("aaaa");
+				navigator.camera.getPicture(onSuccess, onFail, { quality: 50, destinationType: Camera.DestinationType.FILE_URI });
+				console.log("bbbb");
+				function onSuccess(imageURI) {
+				   // var image = document.getElementById('myImage');
+				   // image.src = imageURI;
+				   //target.css("background-image", imageURI);
+				   console.log("cccc");
+				   //$("body").css("background-image", imageURI);
 				   $("#red").prepend('<img id="theImg" src= ' +imageURI+' height="180" width="100">');
 				}
 
 				function onFail(message) {
-					//
+					console.log("dddd");
 					alert('Failed because: ' + message);
 				}
-				
-				//$("#red").prepend('<img id="theImg" src="logo.png">');//' height="180" width="100">');
-
 			} else {
+				//$("body").prepend('<img id="theImg" src="img/logo.png" />');
+				//$("body").css("background-image", url("img/logo.png"));
+				navigator.notification.vibrate(500);
 				target.fadeOut("slow",function(){						
 				});				
 			}
 		} else {
-			//$("#red").remove("#theImg");
+			$("#red").css('background-image', 'none');
 			$("div").fadeIn("slow");	
 		}
 	})			
